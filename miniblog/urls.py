c02a107f827177f920d43afbe704443be2e1c7ed
avinaw20 <avinaw20@gmail.com>
2023-06-24 16:09:53 +0530

    Template is ready

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('', views.home),
     path('about/', views.about , name='about'),
     path('contact/', views.contact , name='contact'),
+    path('dashboard/', views.dashboard , name='dashboard'),
+    path('signup/', views.signup , name='signup'),
+    path('login/', views.user_login , name='login'),
+    path('/logout', views.user_logout , name='logout'),
 ]
