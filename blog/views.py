05a526d71c4e189ee337c6f2805dbe3d30ee1f40
avinaw20 <avinaw20@gmail.com>
2023-06-24 18:41:25 +0530

    Login Authentication Done

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render,HttpResponsePermanentRedirect
+from django.shortcuts import render,HttpResponsePermanentRedirect, redirect
 from .forms import SignUpForm, LoginForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from .models import Post
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    posts = Post.objects.all()
+    return render(request,'blog/home.html',{'posts':posts})
 
 
 def about(request):
@@ -13,9 +16,13 @@ def contact(request):
     return render(request,'blog/contact.html')
 
 def dashboard(request):
-    return render(request,'blog/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request,'blog/dashboard.html')
+    else:
+        return HttpResponsePermanentRedirect('/login/')
 
 def user_logout(request):
+    logout(request)
     return HttpResponsePermanentRedirect('/')
 
 def user_signup(request):
@@ -29,5 +36,19 @@ def user_signup(request):
     return render(request,'blog/signup.html',{'form':form})
 
 def user_login(request):
-    form = LoginForm()
-    return render(request,'blog/login.html',{'form':form})
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request=request, data = request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(username = uname, password = upass)
+                if user is not None:
+                    login(request,user)
+                    messages.success(request,'Logged In successfully')
+                    return HttpResponsePermanentRedirect('/dashboard/')
+        else:
+            form = LoginForm() 
+        return render(request,'blog/login.html',{'form':form})
+    else:# yaani pehle se login h to
+        return HttpResponsePermanentRedirect('/dashboard/')
