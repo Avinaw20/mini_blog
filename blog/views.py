fcc5526183455201f5470a73c61e0cfd2fb3b62f
avinaw20 <avinaw20@gmail.com>
2023-06-24 17:17:05 +0530

    Sign Up Form Successful Submission Done

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponsePermanentRedirect
 from .forms import SignUpForm, LoginForm
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'blog/home.html')
@@ -18,7 +19,13 @@ def user_logout(request):
     return HttpResponsePermanentRedirect('/')
 
 def user_signup(request):
-    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request,'Congratulations !! You have become an Author.')
+            form.save()
+    else :
+        form = SignUpForm()
     return render(request,'blog/signup.html',{'form':form})
 
 def user_login(request):
