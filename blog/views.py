f7c838f77ef82eccc92baefa1d6c7c8b702a6c50
avinaw20 <avinaw20@gmail.com>
2023-06-25 12:06:16 +0530

    Add Post , Edit Post , Delete Post Creation Done

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponsePermanentRedirect, redirect
-from .forms import SignUpForm, LoginForm
+from .forms import SignUpForm, LoginForm, PostForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from .models import Post
+from django.contrib.auth.models import Group
 # Create your views here.
 def home(request):
     posts = Post.objects.all()
@@ -17,7 +18,8 @@ def contact(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request,'blog/dashboard.html')
+        posts = Post.objects.all()
+        return render(request,'blog/dashboard.html',{'posts':posts})
     else:
         return HttpResponsePermanentRedirect('/login/')
 
@@ -30,7 +32,8 @@ def user_signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             messages.success(request,'Congratulations !! You have become an Author.')
-            form.save()
+            user = form.save()
+            
     else :
         form = SignUpForm()
     return render(request,'blog/signup.html',{'form':form})
@@ -52,3 +55,45 @@ def user_login(request):
         return render(request,'blog/login.html',{'form':form})
     else:# yaani pehle se login h to
         return HttpResponsePermanentRedirect('/dashboard/')
+
+
+def add_post(request):
+    if request.user.is_authenticated:
+        form = PostForm()  # Set a default form instance
+
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data['title']
+                desc = form.cleaned_data['desc']
+                pst = Post(title=title, desc=desc)
+                pst.save()
+                form = PostForm()  # Reset the form after saving the post
+
+        return render(request, 'blog/addpost.html', {'form': form})
+    else:
+        return HttpResponsePermanentRedirect('/login/')
+
+    
+def update_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk = id)
+            form = PostForm(request.POST, instance=pi)
+            if form.is_valid():
+                form.save()
+        else:
+            pi = Post.objects.get(pk = id)
+            form = PostForm(instance=pi)
+        return render(request,'blog/updatepost.html',{'form':form})
+    else:
+        return HttpResponsePermanentRedirect('/login/')
+
+def delete_post(request,id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            pi = Post.objects.get(pk = id)
+            pi.delete()
+            return HttpResponsePermanentRedirect('/dashboard/')
+    else:
+        return HttpResponsePermanentRedirect('/login/')
